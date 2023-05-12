b5010d0acfffe5c749117925c288e3d509f7b2f0
Pyrix25633 <biral.mattia@gmail.com>
2023-05-12 13:56:53 +0200

    Working on user authentication

--- pages/js/register.js
+++ pages/js/register.js
@@ -1,14 +1,22 @@
 const registerSectionDiv = document.getElementById('register-section');
 const usernameInput = document.getElementById('username');
+const usernameFeedbackSpan = document.getElementById('username-feedback');
 const emailInput = document.getElementById('email');
+const emailFeedbackSpan = document.getElementById('email-feedback');
 const passwordInput = document.getElementById('password');
+const passwordFeedbackSpan = document.getElementById('password-feedback');
 const registerButton = document.getElementById('register');
+
+let validUsername = false;
+let validEmail = false;
+let validPassword = false;
 
 const confirmSectionDiv = document.getElementById('confirm-section');
 
 confirmSectionDiv.style.display = 'none';
 
 registerButton.addEventListener('click', async () => {
+    if(!(validUsername && validEmail && validPassword)) return;
     $.ajax({
         url: '/api/user/register',
         method: 'POST',
@@ -27,6 +35,99 @@ registerButton.addEventListener('click', async () => {
         }
     });
 });
+
+let usernameTimer;
+usernameInput.addEventListener('keyup', () => {
+    clearTimeout(usernameTimer);
+    usernameTimer = setTimeout(usernameTyped, 1000);
+});
+usernameInput.addEventListener('keydown', () => {
+    clearTimeout(usernameTimer);
+});
+function usernameTyped() {
+    usernameFeedbackSpan.classList.add('success');
+    let username = usernameInput.value;
+    if(username.length < 4) {
+        usernameFeedbackSpan.innerText = 'Username too short!';
+        usernameFeedbackSpan.classList.replace('success', 'error');
+        validUsername = false;
+        return;
+    }
+    if(username.length > 32) {
+        usernameFeedbackSpan.innerText = 'Username too long!';
+        usernameFeedbackSpan.classList.replace('success', 'error');
+        validUsername = false;
+        return;
+    }
+    for(let i = 0; i < username.length; i++) {
+        let c = username.codePointAt(i);
+        if(!((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c == '-' || c == '_'))) {
+            usernameFeedbackSpan.innerText = 'Username contains forbidden character!';
+            validUsername = false;
+            usernameFeedbackSpan.classList.replace('success', 'error');
+            return;
+        }
+    }
+    $.ajax({
+        url: '/api/user/username-used?username=' + username,
+        method: 'GET',
+        dataType: 'json',
+        success: (ret) => {
+            validUsername = ret.used;
+            if(validUsername) {
+                usernameFeedbackSpan.innerText = 'Valid username';
+                usernameFeedbackSpan.classList.replace('error', 'success');
+                return;
+            }
+            usernameFeedbackSpan.innerText = 'Username already taken!';
+            usernameFeedbackSpan.classList.replace('success', 'error');
+        },
+        error: (req, err) => {
+            console.log(err);
+            usernameFeedbackSpan.innerText = 'Server unreachable!';
+            usernameFeedbackSpan.classList.replace('success', 'error');
+        }
+    });
+}
+
+let emailTimer;
+emailInput.addEventListener('keyup', () => {
+    clearTimeout(emailTimer);
+    emailTimer = setTimeout(emailTyped, 1000);
+});
+emailInput.addEventListener('keydown', () => {
+    clearTimeout(emailTimer);
+});
+function emailTyped() {
+    emailFeedbackSpan.classList.add('success');
+    let email = emailInput.value;
+    if(!email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+        emailFeedbackSpan.innerText = 'Invalid email!';
+        emailFeedbackSpan.classList.replace('success', 'error');
+        validEmail = false;
+        return;
+    }
+    $.ajax({
+        url: '/api/user/email-used?email=' + email,
+        method: 'GET',
+        dataType: 'json',
+        success: (ret) => {
+            validEmail = ret.used;
+            if(validEmail) {
+                emailFeedbackSpan.innerText = 'Valid email';
+                emailFeedbackSpan.classList.replace('error', 'success');
+                return;
+            }
+            emailFeedbackSpan.innerText = 'Email already used!';
+            emailFeedbackSpan.classList.replace('success', 'error');
+        },
+        error: (req, err) => {
+            console.log(err);
+            emailFeedbackSpan.innerText = 'Server unreachable!';
+            emailFeedbackSpan.classList.replace('success', 'error');
+        }
+    });
+}
 
 async function hashPassword(password) {
     const hashBuffer = await window.crypto.subtle.digest("SHA-512", new TextEncoder().encode(password));
