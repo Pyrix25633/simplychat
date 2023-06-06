5a92cf76b754221f1837fe21944f5122488542ef
Pyrix25633 <biral.mattia@gmail.com>
2023-06-06 16:55:51 +0200

    Working on settings

--- pages/js/settings.js
+++ pages/js/settings.js
@@ -1,6 +1,21 @@
 const pfpImage = document.getElementById('pfp');
-const usernameH3 = document.getElementById('username');
-const emailSpan = document.getElementById('email');
+const idSpan = document.getElementById('id');
+const usernameInput = document.getElementById('username');
+const usernameFeedbackSpan = document.getElementById('username-feedback');
+const emailInput = document.getElementById('email');
+const emailFeedbackSpan = document.getElementById('email-feedback');
+const passwordInput = document.getElementById('password');
+const passwordFeedbackSpan = document.getElementById('password-feedback');
+const statusInput = document.getElementById('status');
+const cancelButton = document.getElementById('cancel');
+const saveButton = document.getElementById('save');
+
+let settings;
+let validUsername = false;
+let validEmail = false;
+let validPassword = false;
+let validStatus = false;
+saveButton.disabled = true;
 
 const cachedLogin = JSON.parse(localStorage.getItem('cachedLogin'));
 const statusCodeActions = {
@@ -36,6 +51,14 @@ else {
     });
 }
 
+cancelButton.addEventListener('click', () => {
+    window.location.href = '/settings';
+});
+
+saveButton.addEventListener('click', () => {
+
+});
+
 function getSettings() {
     $.ajax({
         url: '/api/user/get-settings',
@@ -48,7 +71,171 @@ function getSettings() {
 }
 
 function showSettings(res) {
+    settings = res;
     pfpImage.src = './pfps/' + cachedLogin.id + '.' + res.pfpType;
-    usernameH3.innerText = res.username;
-    emailSpan.innerText = res.email;
+    idSpan.innerText = cachedLogin.id;
+    usernameInput.value = res.username;
+    emailInput.value = res.email;
+    statusInput.value = res.status;
+}
+
+let usernameTimer;
+usernameInput.addEventListener('keyup', () => {
+    clearTimeout(usernameTimer);
+    usernameTimer = setTimeout(usernameTyped, 1000);
+});
+usernameInput.addEventListener('keydown', () => {
+    clearTimeout(usernameTimer);
+});
+usernameInput.addEventListener('focusout', () => {
+    clearTimeout(usernameTimer);
+    usernameTyped();
+});
+function usernameTyped() {
+    if(usernameInput.value == settings.username) {
+        usernameFeedbackSpan.classList.remove('error', 'success');
+        usernameFeedbackSpan.innerText = 'You can change your Username';
+        return;
+    }
+    usernameFeedbackSpan.classList.add('error');
+    $.ajax({
+        url: '/api/user/username-feedback?username=' + usernameInput.value,
+        method: 'GET',
+        dataType: 'json',
+        success: (res) => {
+            usernameFeedbackSpan.innerText = res.feedback;
+            if(res.feedback.includes('!')) {
+                usernameFeedbackSpan.classList.replace('success', 'error');
+                validUsername = false;
+                registerButton.disabled = true;
+                return;
+            }
+            usernameFeedbackSpan.classList.replace('error', 'success');
+            validUsername = true;
+            registerButton.disabled = !(validUsername && validEmail && validPassword);
+        },
+        error: (req, err) => {
+            console.log(err);
+            usernameFeedbackSpan.innerText = 'Server unreachable!';
+            usernameFeedbackSpan.classList.replace('success', 'error');
+        }
+    });
+}
+
+let emailTimer;
+emailInput.addEventListener('keyup', () => {
+    clearTimeout(emailTimer);
+    emailTimer = setTimeout(emailTyped, 1000);
+});
+emailInput.addEventListener('keydown', () => {
+    clearTimeout(emailTimer);
+});
+emailInput.addEventListener('focusout', () => {
+    clearTimeout(emailTimer);
+    emailTyped();
+});
+function emailTyped() {
+    if(emailInput.value == settings.email) {
+        emailFeedbackSpan.classList.remove('error', 'success');
+        emailFeedbackSpan.innerText = 'You can change your Email';
+        return;
+    }
+    emailFeedbackSpan.classList.add('error');
+    $.ajax({
+        url: '/api/user/email-feedback?email=' + emailInput.value,
+        method: 'GET',
+        dataType: 'json',
+        success: (res) => {
+            emailFeedbackSpan.innerText = res.feedback;
+            if(res.feedback.includes('!')) {
+                emailFeedbackSpan.classList.replace('success', 'error');
+                validEmail = false;
+                registerButton.disabled = true;
+                return;
+            }
+            emailFeedbackSpan.classList.replace('error', 'success');
+            validEmail = true;
+            registerButton.disabled = !(validUsername && validEmail && validPassword);
+        },
+        error: (req, err) => {
+            console.log(err);
+            emailFeedbackSpan.innerText = 'Server unreachable!';
+            emailFeedbackSpan.classList.replace('success', 'error');
+        }
+    });
+}
+
+let passwordTimer;
+passwordInput.addEventListener('keyup', () => {
+    clearTimeout(passwordTimer);
+    passwordTimer = setTimeout(passwordTyped, 1000);
+});
+passwordInput.addEventListener('keydown', () => {
+    clearTimeout(passwordTimer);
+});
+passwordInput.addEventListener('focusout', () => {
+    clearTimeout(passwordTimer);
+    passwordTyped();
+});
+function passwordTyped() {
+    passwordFeedbackSpan.classList.add('error');
+    if(passwordInput.value.length < 4) {
+        passwordFeedbackSpan.innerText = 'Password too short!'
+        passwordFeedbackSpan.classList.replace('success', 'error');
+        validPassword = false;
+        registerButton.disabled = true;
+        return;
+    }
+    if(passwordInput.value.length > 32) {
+        passwordFeedbackSpan.innerText = 'Password too long!'
+        passwordFeedbackSpan.classList.replace('success', 'error');
+        validPassword = false;
+        registerButton.disabled = true;
+        return;
+    }
+    let numbers = 0, symbols = 0, uppercase = 0;
+    for(let i = 0; i < passwordInput.value.length; i++) {
+        let c = passwordInput.value.codePointAt(i);
+        if(c >= 48 && c <= 57) numbers++;
+        else if((c >= 33 && c <= 47) || (c >= 58 && c <= 64) || (c >= 91 && c <= 96) || (c >= 123 && c <= 126)) symbols++;
+        else if(c >= 65 && c <= 90) uppercase++;
+        else if(!(c >= 97 && c <= 122)) {
+            passwordFeedbackSpan.innerText = 'Password contains forbidden character!'
+            passwordFeedbackSpan.classList.replace('success', 'error');
+            validPassword = false;
+            registerButton.disabled = true;
+            return;
+        }
+    }
+    if(numbers < 2) {
+        passwordFeedbackSpan.innerText = 'Password should contain at least 2 numbers!'
+        passwordFeedbackSpan.classList.replace('success', 'error');
+        validPassword = false;
+        registerButton.disabled = true;
+    }
+    else if(symbols < 1) {
+        passwordFeedbackSpan.innerText = 'Password should contain at least 1 symbol!'
+        passwordFeedbackSpan.classList.replace('success', 'error');
+        validPassword = false;
+        registerButton.disabled = true;
+    }
+    else if(uppercase < 2) {
+        passwordFeedbackSpan.innerText = 'Password should contain at least 2 uppercase letters!'
+        passwordFeedbackSpan.classList.replace('success', 'error');
+        validPassword = false;
+        registerButton.disabled = true;
+    }
+    else {
+        passwordFeedbackSpan.innerText = 'Valid password'
+        passwordFeedbackSpan.classList.replace('error', 'success');
+        validPassword = true;
+        registerButton.disabled = !(validUsername && validEmail && validPassword);
+    }
+}
+
+async function hashPassword(password) {
+    const hashBuffer = await window.crypto.subtle.digest("SHA-512", new TextEncoder().encode(password));
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+    return hashHex;
 }
